929232ed8f877bc9d739006ef96e13800825e93d
Benjamín Bello <benjamin.bello@sansano.usm.cl>
2024-12-17 14:03:03 -0300

    Add initial implementation of dice rolling action with GitHub Actions

---
+++ index.js
@@ -0,0 +1,23 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const tiradaDado = () => {
+
+    return Math.floor((Math.random() * 6) + 1)
+
+}
+
+try {
+    const payloadEvent = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`Payload event: ${payloadEvent}`);
+
+    const numeroDados = core.getInput('numero-dados');
+    core.notice(`Numero de dados: ${numeroDados}`);
+    core.setOutput('dado1', tiradaDado());
+    if (numeroDados == 2) {
+        core.setOutput('dado2', tiradaDado());
+    }
+
+} catch (error) {
+    core.setFailed(error.message)
+}
